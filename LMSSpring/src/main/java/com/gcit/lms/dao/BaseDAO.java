57fe4e1fb9bd9ed6445f10c84003b81258f9ff9a
Yikai Cao <yikai_cao@gcitsolutions.com>
2017-05-19 23:28:42 -0400

    Deleted mongodb configuration.

--- LMSSpring/src/main/java/com/gcit/lms/dao/BaseDAO.java
+++ LMSSpring/src/main/java/com/gcit/lms/dao/BaseDAO.java
@@ -1,16 +1,12 @@
 package com.gcit.lms.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public abstract class BaseDAO {
 	
 	@Autowired
 	public JdbcTemplate template;
-	
-	@Autowired
-	public MongoTemplate mongoTemplate;
 	
 	@Autowired
 	public SequenceDAO seqDao;
